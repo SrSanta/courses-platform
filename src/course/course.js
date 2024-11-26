b8e31e432f4f71501c6e2b5c66c204bf3cca5193
Andrés <andrescondegarcia11@gmail.com>
2024-11-26 16:46:38 +0100

    Creating the html file structure for the course and course class and their constructors.

---
+++ src/course/course.js
@@ -0,0 +1,11 @@
+class Course {
+    constructor(id, name, description, duration, coordinates, finished, idStudent) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.duration = duration;
+        this.coordinates = coordinates;
+        this.finished = finished;
+        this.idStudent = idStudent;
+    }
+}
