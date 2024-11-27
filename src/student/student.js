358a8459367321514aa82f1808c312d0fa1e1e48
Andrés <andrescondegarcia11@gmail.com>
2024-11-27 19:49:13 +0100

    Modification of the listStudent view, repair of the functions of student.js createStudent because if there was an element in the array it didn't work, creation of the function deleteStudent by id, and expert all the function to use in the other .js files. created the logic to display the information from the student table and delete the student.

--- src/student/student.js
+++ src/student/student.js
@@ -10,20 +10,53 @@ class Student {
 }
 
 //create array to load the students from students.json
-let students = [];
+let students = [{
+    id: 1,
+    name: "Andrés",
+    age: 21,
+    address: "Calle",
+    onGoing: false
+}];
 
 //funtion to create new student
-function createStudent(name, age, address){
-    const id = 1;
-    if (students.length) {
-        id = students[students.length-1].id + 1;
+function createStudent(name, age, address) {
+    let id = 1;
+    if (students.length === 0) {
+        id = 1;
+    } else {
+        id = students[students.length - 1].id + 1;
     }
 
-    const newStudent = new Student(id, name,age, address);
+    const newStudent = new Student(id, name, age, address, false);
     students.push(newStudent);
 };
 
 //funtion to list all the students
-function listStudents(){
+function listStudents() {
     return students;
 };
+console.log(listStudents());
+
+//funtion to edit existing student
+function editStudent(id, name, age, address, onGoing) {
+    const studentExist = students.find(student => student.id === id);
+    if (studentExist) {
+        studentExist.name = name;
+        studentExist.age = age;
+        studentExist.address = address;
+        studentExist.onGoing = onGoing;
+    };
+};
+
+//funtion to delete student by id
+function deleteStudent(id) {
+    const position = students.findIndex(student => student.id === id);
+
+    console.log(position);
+
+    if (position !== -1) {
+        students.splice(position, 1);
+    };
+};
+
+export { Student, listStudents, createStudent, editStudent, deleteStudent };
