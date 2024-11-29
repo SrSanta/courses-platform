785442794afe41ac5ff1bd3e752244a6511cf415
Andrés <andrescondegarcia11@gmail.com>
2024-11-29 17:39:40 +0100

    Final change to course

--- src/course/chargeCourse.js
+++ src/course/chargeCourse.js
@@ -1,15 +1,13 @@
 //logic for updating the table of courses
 // Import required course.js functions
-import { Course, listCourses, createCourse, deleteCourse } from "./course.js";
+import {Course} from "./course.js";
+import {courses} from "../assets/courses.js";
 
 // Create a test course
 createCourse("JavaScript Basics", "Learn the basics of JavaScript", "20 hours", { lat: 40.7128, lng: -74.0060 }, false, 1);
 
-// Load courses with the listCourses() function from course.js
-const courses = listCourses();
-
 // Function for updating the course table
-function loadCourses() {
+window.onload = function loadCourses() {
     const tablaBody = document.getElementById("tableCourse");
     tablaBody.innerHTML = "";
 
@@ -22,7 +20,7 @@ function loadCourses() {
             <td>${course.duration}</td>
             <td>${course.coordinates.lat}, ${course.coordinates.lng}</td>
             <td>${course.finished ? "Yes" : "No"}</td>
-            <td>${course.idStudent}</td>
+            <td>${course.idStudents}</td>
             <td>
                 <div>
                     <button class="btn btn-danger btn-sm">Delete</button>
@@ -38,4 +36,24 @@ function loadCourses() {
     });
 }
 
-loadCourses();
+//Function to create a new course
+function createCourse(name, description, duration, coordinates, finished, idStudents) {
+    let id = 1;
+    if (courses.length === 0) {
+        id = 1;
+    } else {
+        id = courses[courses.length - 1].id + 1;
+    }
+
+    const newCourse = new Course(id, name, description, duration, coordinates, finished, idStudents);
+    courses.push(newCourse);
+}
+
+//Function to delete a course by ID
+function deleteCourse(id) {
+    const position = courses.findIndex(course => course.id === id);
+
+    if (position !== -1) {
+        courses.splice(position, 1);
+    }
+}
--- src/course/course.js
+++ src/course/course.js
@@ -11,39 +11,4 @@ class Course {
     }
 }
 
-// Función para crear un nuevo curso
-function createCourse(name, description, duration, coordinates, finished, idStudents) {
-    let id = 1;
-    if (courses.length === 0) {
-        id = 1;
-    } else {
-        id = courses[courses.length - 1].id + 1;
-    }
-
-    const newCourse = new Course(id, name, description, duration, coordinates, finished, idStudents);
-    courses.push(newCourse);
-}
-
-// Función para editar un curso existente
-function editCourse(id, name, description, duration, coordinates, finished, idStudents) {
-    const courseExist = courses.find(course => course.id === id);
-    if (courseExist) {
-        courseExist.name = name;
-        courseExist.description = description;
-        courseExist.duration = duration;
-        courseExist.coordinates = coordinates;
-        courseExist.finished = finished;
-        courseExist.idStudents = idStudents;
-    }
-}
-
-// Función para eliminar un curso por ID
-function deleteCourse(id) {
-    const position = courses.findIndex(course => course.id === id);
-
-    if (position !== -1) {
-        courses.splice(position, 1);
-    }
-}
-
-export{Course, createCourse, editCourse, deleteCourse};
+export{Course};
